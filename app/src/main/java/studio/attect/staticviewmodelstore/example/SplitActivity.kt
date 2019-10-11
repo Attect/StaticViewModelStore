df86aa80dd0bd66b91d171291af7891f7620741c
attect <412966721@qq.com>
2019-10-11 20:40:34 +0800

    升级gradle/kotlin，修复demo输入框重复及折叠屏下的表现

--- app/src/main/java/studio/attect/staticviewmodelstore/example/SplitActivity.kt
+++ app/src/main/java/studio/attect/staticviewmodelstore/example/SplitActivity.kt
@@ -8,7 +8,9 @@ class SplitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_split)
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerTop,TopFragment()).add(R.id.fragmentContainerBottom,BottomFragment()).commit()
+        if(supportFragmentManager.findFragmentById(R.id.fragmentContainerTop)== null){
+            supportFragmentManager.beginTransaction().add(R.id.fragmentContainerTop,TopFragment()).add(R.id.fragmentContainerBottom,BottomFragment()).commit()
+        }
     }
 
     override fun onStart() {
